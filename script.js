54d1a872deba5546ba31a317c51d22feeef66c90
LAÍS CIPRIANO <151071877+ciprilais@users.noreply.github.com>
2024-04-23 20:32:42 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function addRegister() {
+    const email = document.getElementById('email').value 
+    const password = document.getElementById('password').value 
+
+    if(email === 'admin@admin.com', password === '1234') {
+        alert('Login successful')
+    } else {
+        alert('Login failed')
+    }
+}
